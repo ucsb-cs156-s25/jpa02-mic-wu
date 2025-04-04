b3d2dffac73a84457c456e8c3d512e6acf45f38f
Michael Wu <michael_wu@ucsb.edu>
2025-04-03 17:31:14 -0700

    mw - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -28,5 +28,10 @@ public class DeveloperTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        // <https://bit.ly/cs156-s25-teams>
+        assertEquals("mic-wu", Developer.getGithubId());
+    }
 
 }
